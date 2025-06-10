c2ec827cf0a1cf8af5d4dfaa2abf6050756ec0ce
Priya15-cp <pb07602@gmail.com>
2025-06-10 22:52:48 +0530

    Add files via upload

---
+++ operation.js
@@ -0,0 +1,23 @@
+// // array type
+// // let arry=[1,4,7,9,5,4,6];
+// // let off=['hello','making','maria'];
+// // // concatinate ny concat method
+// // let dd=(arry.concat(off));
+// // console.log(dd);
+// // //concatinate by template littral
+// // console.log(`${arry} ${off}`);
+// let arry=[1,4,7,9,5,4,6];
+// arry.sort();
+// console.log(arry);
+// arry.reverse();
+// console.log(arry);
+// arry.find();
+
+// foreach mathod( used for performing operatiom on all element)
+let ss=[4,9,6,4,6];
+let length=ss.length;
+console.log(length);
+ss.forEach((value ,index) => {
+    console.log("number", value, "index", index);
+    
+});
---
+++ variables.js
@@ -0,0 +1,19 @@
+var hello=2
+var hello=3
+console.log(hello)
+/*var can be re_declared and updated 
+ -it is globle scope variable */
+
+ let digit=5
+digit=4
+console.log(digit)
+/*let can be updated but cannt re-declared
+it is block scope variable */
+
+const age=9
+// age =4 -- it generate error because const cannt be redeclared and updated
+console.log(age)
+
+console.log(typeof(age))
+/*typeof gives the datatype of variable like object , number,strings,undefined */
+
