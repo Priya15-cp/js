f0b9b598b149985fe25f6770fb4ee63b9645f364
Priya15-cp <pb07602@gmail.com>
2025-06-11 21:49:24 +0530

    Add files via upload

---
+++ operations.js
@@ -0,0 +1,22 @@
+let v1=3
+let v2=5
+// in arthematic there is 
+/* + = add
+   - = substract 
+    ++a = 1st increment the print
+    a++ = 1st print then increment
+    % --> return remainder
+    ** --> power
+    ==(losses equality) --> gives true if both value is same
+    ===(strict eqality) --> gives true when both type and value is true
+
+     */
+console.log(v1**v2)// gives 3*3*3*3*3
+
+
+console.log("2"+3+4)
+// if string is in 1st then it takes all ther value as string
+console.log(2+"3"+4)
+// same as above
+console.log(2+3+"4")
+// it gives output 54 -- it add number 2 and 3 and string "4" as it is
