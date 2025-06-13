a04c0f99a9670f8aaaf33c622050c6ae4fa737cf
Priya15-cp <pb07602@gmail.com>
2025-06-13 21:44:12 +0530

    Delete datatypeconversion.js

--- datatypeconversion.js
+++
@@ -1,9 +0,0 @@
-let age ="17"
-
-console.log(typeof(age))
-// coversion from string to number
-let age_as_number= Number(age)
-console.log(typeof(age_as_number))
-/* in boolen true=1 and false=0
-   "" == false  
-   if string like""prita " ==true*/
